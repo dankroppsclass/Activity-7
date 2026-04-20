0a0c22e2c794d34fde1bcc0aec258640fc2924b5
dankroppsclass <dgking@hamilton.edu>
2026-04-20 20:30:23 +0000

    class activity

---
+++ a7.R
@@ -0,0 +1,133 @@
+# read in greenhouse gas data from reservoirs
+ghg <- read.csv("/cloud/project/activity07/Deemer_GHG_Data.csv")
+install.packages(c("dplyr","ggplot2","olsrr", "PerformanceAnalytics", "lubridate", "forecast"))
+library(dplyr)
+library(ggplot2)
+library(olsrr)
+library(PerformanceAnalytics)
+
+# log transform methane fluxes
+ghg$log.ch4 <- log(ghg$ch4+1)
+ghg$log.age <- log(ghg$age)
+ghg$log.DIP <- log(ghg$DIP+1)
+ghg$log.precip <- log(ghg$precipitation)
+ghg$log.runoff = log(ghg$runoff+1)
+unique(ghg$Region)
+
+# binary variable for boreal region
+ghg$BorealV <- ifelse(ghg$Region == "Boreal",1,0)
+# binary variable for tropical region
+ghg$TropicalV <- ifelse(ghg$Region == "Tropical",1,0)
+# binary variable for alpine region
+ghg$AlpineV <- ifelse(ghg$Alpine == "yes",1,0)
+# binary variable for known hydropower
+ghg$HydroV <- ifelse(ghg$hydropower == "yes",1,0)
+
+# multiple regression
+# creates a model object
+mod.full <- lm(log.ch4 ~ airTemp+
+                 log.age+
+                 mean.depth+
+                 log.DIP+
+                 log.precip+
+                 log.runoff+
+                 BorealV, data=ghg) #uses the data argument to specify dataframe
+
+summary(mod.full)
+
+res.full <- rstandard(mod.full)
+fit.full <- fitted.values(mod.full)
+
+# qq plot
+qqnorm(res.full, pch=19, col="grey50")
+qqline(res.full)
+
+# shapiro-wilks test
+shapiro.test(res.full)
+
+#residuals
+plot(fit.full,res.full, pch=19, col="grey50")
+abline(h=0)
+
+# isolate continuous model variables into data frame:
+reg.data <- data.frame(ghg$airTemp,
+                       ghg$log.age,
+                       ghg$mean.depth,
+                       ghg$log.DIP,
+                       ghg$log.precip,
+                       ghg$log.runoff)
+
+# make a correlation matrix 
+chart.Correlation(reg.data, histogram=TRUE, pch=19)
+
+# run stepwise
+full.step <- ols_step_forward_aic(mod.full)
+# view table
+full.step 
+
+# check full model
+full.step$model
+
+# plot AIC over time
+plot(full.step )
+
+# prediction with interval for predicting a point
+predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
+                                mean.depth=15,log.DIP=3,
+                                log.precip=6, log.runoff=200, BorealV=0),
+           interval="prediction")
+
+# look at prediction with 95% confidence interval of the mean
+predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
+                                mean.depth=15,log.DIP=3,
+                                log.precip=6, log.runoff=200, BorealV=0),
+           interval="confidence")
+
+#####Homework prompts
+
+ETdat <- read.csv("/cloud/project/activity07/ETdata.csv")
+
+unique(ETdat$crop)
+
+library(forecast)
+library(lubridate)
+
+# average fields for each month for almonds
+almond <- ETdat %>% # ET data
+  filter(crop == "Almonds") %>% # only use almond fields
+  group_by(date) %>% # calculate over each date
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
+
+# visualize the data
+ggplot(almond, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
+
+# almond ET time series
+almond_ts <- ts(almond$ET.in, # data
+                start = c(2016,1), #start year 2016, month 1
+                #first number is unit of time and second is observations within a unit
+                frequency= 12) # frequency of observations in a unit
+
+# decompose almond ET time series
+almond_dec <- decompose(almond_ts)
+# plot decomposition
+plot(almond_dec)
+
+almondTrend <- almond_dec$trend
+almondSeason <- almond_dec$seasonal
+
+acf(na.omit(almond_ts), # remove missing data
+    lag.max = 24) # look at 2 years (24 months)
+
+pacf.plot <- pacf(na.omit(almond_ts))
+
+almond_y <- na.omit(almond_ts)
+model1 <- arima(almond_y , # data 
+                order = c(1,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model1
+
+model4 <- arima(almond_y , # data 
+                order = c(4,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model4
