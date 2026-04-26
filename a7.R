ea0e1c1e0c9373b89ca5a515fccafe3b0826f6c0
dankroppsclass <dgking@hamilton.edu>
2026-04-26 01:51:48 +0000

    final hw7

--- a7.R
+++ a7.R
@@ -1,3 +1,4 @@
+####Greenhouse Gas####
 # read in greenhouse gas data from reservoirs
 ghg <- read.csv("/cloud/project/activity07/Deemer_GHG_Data.csv")
 install.packages(c("dplyr","ggplot2","olsrr", "PerformanceAnalytics", "lubridate", "forecast"))
@@ -83,8 +84,8 @@ predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
                                 log.precip=6, log.runoff=200, BorealV=0),
            interval="confidence")
 
-#####Homework prompts
 
+####Almond Fields####
 ETdat <- read.csv("/cloud/project/activity07/ETdata.csv")
 
 unique(ETdat$crop)
@@ -131,3 +132,427 @@ model1
 model4 <- arima(almond_y , # data 
                 order = c(4,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
 model4
+
+# calculate fit
+AR_fit1 <- almond_y - residuals(model1) 
+AR_fit4 <- almond_y - residuals(model4)
+#plot data
+plot(almond_y)
+# plot fit
+points(AR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(AR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("topleft", c("data","AR1","AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3","darkgoldenrod4"),
+       bty="n")
+
+newAlmond <- forecast(model4)
+newAlmond
+
+#make dataframe for plotting
+newAlmondF <- data.frame(newAlmond)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newAlmondF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# make a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = almond, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(almond$date[1]),newAlmondF$dateF[24])+  # Plotting original data
+  geom_line(data = newAlmondF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  # Plotting model forecasts
+  geom_ribbon(data=newAlmondF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ # uncertainty interval
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)")
+
+
+####Homework Prompts####
+####Question 1####
+# log transform methane fluxes
+ghg$log.ch4 <- log(ghg$ch4+1)
+ghg$log.age <- log(ghg$age)
+ghg$log.DIP <- log(ghg$DIP+1)
+ghg$log.precip <- log(ghg$precipitation)
+ghg$log.runoff = log(ghg$runoff+1)
+ghg$log.surface.area = log(ghg$surface.area)
+ghg$trans.co2 = 1/(ghg$co2+1000)
+unique(ghg$Region)
+
+# binary variable for boreal region
+ghg$BorealV <- ifelse(ghg$Region == "Boreal",1,0)
+# binary variable for tropical region
+ghg$TropicalV <- ifelse(ghg$Region == "Tropical",1,0)
+# binary variable for alpine region
+ghg$AlpineV <- ifelse(ghg$Alpine == "yes",1,0)
+# binary variable for known hydropower
+ghg$HydroV <- ifelse(ghg$hydropower == "yes",1,0)
+
+# multiple regression
+# creates a model object
+co2mod.full <- lm(trans.co2 ~ 
+                    log.age+mean.depth+
+                    log.DIP+
+                    log.precip+
+                    BorealV, data=ghg) #uses the data argument to specify dataframe
+
+summary(co2mod.full)
+
+nobs(co2mod.full)
+
+co2res.full <- rstandard(co2mod.full)
+co2fit.full <- fitted.values(co2mod.full)
+
+# qq plot
+qqnorm(co2res.full, pch=19, col="grey50")
+qqline(co2res.full)
+
+# shapiro-wilks test
+shapiro.test(co2res.full)
+
+#residuals
+plot(co2fit.full,co2res.full, pch=19, col="grey50", main="Residuals")
+abline(h=0)
+
+# isolate continuous model variables into data frame:
+reg.data <- data.frame(ghg$airTemp,
+                       ghg$log.age,
+                       ghg$mean.depth,
+                       ghg$log.DIP,
+                       ghg$log.precip,
+                       ghg$log.runoff)
+
+# make a correlation matrix 
+chart.Correlation(reg.data, histogram=TRUE, pch=19)
+
+# run stepwise
+co2full.step <- ols_step_forward_aic(co2mod.full)
+
+# view table
+co2full.step 
+
+# check full model
+co2full.step$model
+
+# plot AIC over time
+plot(co2full.step )
+
+# prediction with interval for predicting a point
+predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
+                                mean.depth=15,log.DIP=3,
+                                log.precip=6, log.runoff=200, BorealV=0),
+           interval="prediction")
+
+# look at prediction with 95% confidence interval of the mean
+predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
+                                mean.depth=15,log.DIP=3,
+                                log.precip=6, log.runoff=200, BorealV=0),
+           interval="confidence")
+
+
+
+####Question 3####
+##Pistachio Decomposition##
+# average fields for each month for almonds
+unique(ETdat$crop)
+
+pistachio <- ETdat %>% # ET data
+  filter(crop == "Pistachios") %>% # only use almond fields
+  group_by(date) %>% # calculate over each date
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
+
+# visualize the data
+ggplot(pistachio, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
+
+# almond ET time series
+pistachio_ts <- ts(pistachio$ET.in, # data
+                start = c(2016,1), #start year 2016, month 1
+                #first number is unit of time and second is observations within a unit
+                frequency= 12) # frequency of observations in a unit
+
+# decompose almond ET time series
+pistachio_dec <- decompose(pistachio_ts)
+# plot decomposition
+plot(pistachio_dec)
+
+##Fallow Fields
+
+unique(ETdat$crop)
+
+fallow <- ETdat %>% # ET data
+  filter(crop == "Fallow/Idle Cropland") %>% # only use almond fields
+  group_by(date) %>% # calculate over each date
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
+
+# visualize the data
+ggplot(fallow, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
+
+# almond ET time series
+fallow_ts <- ts(fallow$ET.in, # data
+                   start = c(2016,1), #start year 2016, month 1
+                   #first number is unit of time and second is observations within a unit
+                   frequency= 12) # frequency of observations in a unit
+
+# decompose almond ET time series
+fallow_dec <- decompose(fallow_ts)
+# plot decomposition
+plot(fallow_dec)
+
+##Corn##
+
+unique(ETdat$crop)
+
+corn <- ETdat %>% # ET data
+  filter(crop == "Corn") %>% # only use almond fields
+  group_by(date) %>% # calculate over each date
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
+
+# visualize the data
+ggplot(corn, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
+
+# almond ET time series
+corn_ts <- ts(corn$ET.in, # data
+                start = c(2016,1), #start year 2016, month 1
+                #first number is unit of time and second is observations within a unit
+                frequency= 12) # frequency of observations in a unit
+
+# decompose almond ET time series
+corn_dec <- decompose(corn_ts)
+# plot decomposition
+plot(corn_dec)
+
+##Grape Decomposition##
+
+unique(ETdat$crop)
+
+grape <- ETdat %>% # ET data
+  filter(crop == "Grapes (Table/Raisin)") %>% # only use almond fields
+  group_by(date) %>% # calculate over each date
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
+
+# visualize the data
+ggplot(grape, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
+
+# almond ET time series
+grape_ts <- ts(grape$ET.in, # data
+                start = c(2016,1), #start year 2016, month 1
+                #first number is unit of time and second is observations within a unit
+                frequency= 12) # frequency of observations in a unit
+
+# decompose almond ET time series
+grape_dec <- decompose(grape_ts)
+# plot decomposition
+plot(grape_dec)
+
+
+##Almond Decomposition##
+
+# average fields for each month for almonds
+almond <- ETdat %>% # ET data
+  filter(crop == "Almonds") %>% # only use almond fields
+  group_by(date) %>% # calculate over each date
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
+
+# visualize the data
+ggplot(almond, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
+
+# almond ET time series
+almond_ts <- ts(almond$ET.in, # data
+                start = c(2016,1), #start year 2016, month 1
+                #first number is unit of time and second is observations within a unit
+                frequency= 12) # frequency of observations in a unit
+
+# decompose almond ET time series
+almond_dec <- decompose(almond_ts)
+# plot decomposition
+plot(almond_dec)
+
+#Almond Forcast
+
+almondTrend <- almond_dec$trend
+almondSeason <- almond_dec$seasonal
+
+acf(na.omit(almond_ts), # remove missing data
+    lag.max = 24) # look at 2 years (24 months)
+
+pacf.plot <- pacf(na.omit(almond_ts))
+
+almond_y <- na.omit(almond_ts)
+model1 <- arima(almond_y , # data 
+                order = c(1,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model1
+
+model4 <- arima(almond_y , # data 
+                order = c(4,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model4
+
+# calculate fit
+AR_fit1 <- almond_y - residuals(model1) 
+AR_fit4 <- almond_y - residuals(model4)
+#plot data
+plot(almond_y)
+# plot fit
+points(AR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(AR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("topleft", c("data","AR1","AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3","darkgoldenrod4"),
+       bty="n")
+
+newAlmond <- forecast(model4)
+newAlmond
+
+#make dataframe for plotting
+newAlmondF <- data.frame(newAlmond)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newAlmondF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# make a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = almond, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(almond$date[1]),newAlmondF$dateF[24])+  # Plotting original data
+  geom_line(data = newAlmondF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  # Plotting model forecasts
+  geom_ribbon(data=newAlmondF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ # uncertainty interval
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)")
+
+
+#Pistachio Forecast
+
+pistachioTrend <- pistachio_dec$trend
+pistachioSeason <- pistachio_dec$seasonal
+
+acf(na.omit(pistachio_ts), # remove missing data
+    lag.max = 24) # look at 2 years (24 months)
+
+pacf.plot <- pacf(na.omit(pistachio_ts))
+
+pistachio_y <- na.omit(pistachio_ts)
+pmodel1 <- arima(pistachio_y , # data 
+                order = c(1,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+pmodel1
+
+pmodel4 <- arima(pistachio_y , # data 
+                order = c(4,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+pmodel4
+
+# calculate fit
+PR_fit1 <- pistachio_y - residuals(pmodel1) 
+PR_fit4 <- pistachio_y - residuals(pmodel4)
+#plot data
+plot(pistachio_y)
+# plot fit
+points(PR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(PR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("topleft", c("data","PR1","PR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3","darkgoldenrod4"),
+       bty="n")
+
+newpistachio <- forecast(pmodel4)
+newpistachio
+
+#make dataframe for plotting
+newpistachioF <- data.frame(newpistachio)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newpistachioF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# make a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = pistachio, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(pistachio$date[1]),newpistachioF$dateF[24])+  # Plotting original data
+  geom_line(data = newpistachioF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  # Plotting model forecasts
+  geom_ribbon(data=newpistachioF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ # uncertainty interval
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)", title="Pistachio Forecast")
+
+
+
+
+#Fallow Forecast
+
+fallowTrend <- fallow_dec$trend
+fallowSeason <- fallow_dec$seasonal
+
+acf(na.omit(fallow_ts), # remove missing data
+    lag.max = 24) # look at 2 years (24 months)
+
+pacf.plot <- pacf(na.omit(fallow_ts))
+
+fallow_y <- na.omit(fallow_ts)
+fmodel1 <- arima(fallow_y , # data 
+                 order = c(1,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+fmodel1
+
+fmodel4 <- arima(fallow_y , # data 
+                 order = c(4,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+fmodel4
+
+# calculate fit
+FR_fit1 <- fallow_y - residuals(fmodel1) 
+FR_fit4 <- fallow_y - residuals(fmodel4)
+#plot data
+plot(fallow_y)
+# plot fit
+points(FR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(FR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("topleft", c("data","FR1","FR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3","darkgoldenrod4"),
+       bty="n")
+
+newfallow <- forecast(fmodel4)
+newfallow
+
+#make dataframe for plotting
+newfallowF <- data.frame(newfallow)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newfallowF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# make a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = fallow, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(fallow$date[1]),newfallowF$dateF[24])+  # Plotting original data
+  geom_line(data = newfallowF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  # Plotting model forecasts
+  geom_ribbon(data=newfallowF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ # uncertainty interval
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)", title="Fallow Forecast")
+
+
+
